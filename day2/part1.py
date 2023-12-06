33ae00363ac25893243ca7d2759602b7f65e2093
jensjvh <43621451+jensjvh@users.noreply.github.com>
2023-12-06 15:59:27 +0200

    Add comments to day 2 solutions

--- day2/part1.py
+++ day2/part1.py
@@ -1,23 +1,31 @@
 with open("input.txt", "r") as inputs:
+    #Represent amount of cubes in the bag in a dictionary
     colours = {"red": 12, "green": 13, "blue": 14}
     res = []
+    
     for game in inputs:
+        #Separate each line in the input file at ":"
         game = game.strip().split(":")
         possible = True
         
+        #Iterate over rounds contained in game[1], split at ", "
         for round in game[1].split("; "):
             round = round.strip().split(", ")
             
             for colour in round:
+                #In rounds, split the number of cubes and the colour
                 colour = colour.split(" ")
+
+                #Check if amount of coloured cubes is bigger
+                #than the amount of cubes in the dictionary for the respective colour
                 if colours[colour[1]] < int(colour[0]):
                     print(f"{game[0]} not possible")
                     possible = False
             
-        
         if possible == False:
             continue
 
+        #If the game is possible, append the ID of the game to the res array
         res.append(int(game[0].split(" ")[1]))
     
     print(sum(res))
--- day2/part2.py
+++ day2/part2.py
@@ -1,18 +1,26 @@
 with open("input.txt", "r") as inputs:
     res = []
     for game in inputs:
+        #Initialise the powers of the cubes to 1
         powers = 1
+        #The colours dictionary is used to keep track of the max value of the coloured cubes
         colours = {"red": 0, "green": 0, "blue": 0}
         game = game.strip().split(":")
         
+        #Iterate over each round in a line, split by "; "
         for round in game[1].split("; "):
             round = round.strip().split(", ")
             
+            #Iterate over each colour in each round
             for colour in round:
                 colour = colour.split(" ")
+                #Check if the number of cubes is greater than the value stored in the dictionary
+                #If yes, set the dictionary value to be equal to the colour
                 if colours[colour[1]] < int(colour[0]):
                     colours[colour[1]] = int(colour[0])
 
+        #Iterate over the dictionary key-value pairs and
+        #multiply the powers variable with each value.
         for _, value in colours.items():
             powers *= value
 
